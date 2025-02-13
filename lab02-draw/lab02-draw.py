dded8eaa698c7740fe7568c2c5b25d769f0cdb48
paveiii <pavel.tabata@edu.uah.es>
2025-02-13 16:21:03 +0100

    THE JACKET DRAWING WIP 2

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -2,7 +2,7 @@ import arcade
 import random
 from arcade import draw_polygon_filled, draw_rectangle_filled
 
-arcade.open_window(800, 600, "Hola")
+arcade.open_window(1980, 1270, "Hola")
 
 #JACKEEEEEEEEEEET
 arcade.set_background_color(arcade.color.BLACK)
@@ -19,11 +19,13 @@ arcade.draw_polygon_filled(mascaraPuntos1, arcade.color.BEIGE)
 mascaraPuntos2 = ((370,550),(400,575),(430,550),(425,400),(375,400))
 arcade.draw_polygon_filled(mascaraPuntos2, arcade.color.ANTIQUE_RUBY)
 
-mascaraPuntos3 = ((275,230),(375,200), (425,200),(525,230),(500,475),(450,550),(350,550),(300,475))
-arcade.draw_polygon_filled(mascaraPuntos1, arcade.color.BEIGE)
+arcade.draw_ellipse_filled()
 
-arcade.draw_point(350,375,arcade.color.BLACK,35)
-arcade.draw_point(450,375,arcade.color.BLACK,35)
+mascaraPuntos3 = ((390,370),(410, 370),(435,300),(400,275),(365,300))
+arcade.draw_polygon_filled(mascaraPuntos3, arcade.color.EARTH_YELLOW)
+
+arcade.draw_point(350,375,arcade.color.BLACK,40)
+arcade.draw_point(450,375,arcade.color.BLACK,40)
 arcade.finish_render()
 
 
