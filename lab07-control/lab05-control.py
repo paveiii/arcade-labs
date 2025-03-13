424965c0cbeb92cb71c45c927d029ad5f51089bb
paveiii <pavel.tabata@edu.uah.es>
2025-03-13 15:26:50 +0100

    Lab 05 Aight I fix the weird keyboard movement, it works without tapping

--- lab07-control/lab05-control.py
+++ lab07-control/lab05-control.py
@@ -44,17 +44,19 @@ def draw_jacket(x,y):
     draw_rectangle_filled(x, y, 400, 250, arcade.color.APRICOT, 0)
     draw_rectangle_filled(x, y, 250, 250, arcade.color.RED_BROWN, 0)
 class Jacket:
-    def __init__(self, position_x, position_y):
+    def __init__(self, position_x, position_y, change_x, change_y):
 
         # Take the parameters of the init function above,
         # and create instance variables out of them.
         self.position_x = position_x
         self.position_y = position_y
+        self.change_x = change_x
+        self.change_y = change_y
     def draw(self):
         draw_jacket(self.position_x, self.position_y)
-    def update_movement(self, change_x = None, change_y = None):
-        if(change_y != None): self.position_y += change_y
-        if(change_x != None):self.position_x += change_x
+    def on_update(self):
+        self.position_y += self.change_y
+        self.position_x += self.change_x
 
         if self.position_x < 200:
             self.position_x = 200
@@ -75,7 +77,7 @@ class MyGame(arcade.Window):
         # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
 
-        self.jacket = Jacket(300,300)
+        self.jacket = Jacket(300,300,0,0)
         self.circle = Circle(300,300)
 
         joysticks = arcade.get_joysticks()
@@ -107,31 +109,27 @@ class MyGame(arcade.Window):
                 self.circle.update_movement(None, 0)
             else:
                 self.circle.update_movement(None, -self.joystick.y * MOVEMENT_SPEED)
+        self.jacket.on_update()
 
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
         if key == arcade.key.LEFT:
-            self.jacket.update_movement(-MOVEMENT_SPEED)
+            self.jacket.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
-            self.jacket.update_movement(MOVEMENT_SPEED)
+            self.jacket.change_x = MOVEMENT_SPEED
         elif key == arcade.key.UP:
-            self.jacket.update_movement(None,MOVEMENT_SPEED)
+            self.jacket.change_y = MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
-            self.jacket.update_movement(None,-MOVEMENT_SPEED)
+            self.jacket.change_y = -MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.jacket.update_movement(0)
+            self.jacket.change_x = 0
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.jacket.update_movement(None,0)
-
-
-
+            self.jacket.change_y = 0
 def main():
     window = MyGame()
     arcade.run()
-
-
 
 main()
