af29f4e5daa0142dab14b1907de28664ef6e2688
paveiii <pavel.tabata@edu.uah.es>
2025-03-13 16:20:08 +0100

    WIP. To do: - Añadir movimiento de enemigos

--- lab08-sprites/lab08-sprites.py
+++ lab08-sprites/lab08-sprites.py
@@ -15,17 +15,16 @@ score_points_list = []
 def draw_fire(p1,p2, color):
     arcade.draw_line(p1 + (random.randint(-400,400)), (random.randint(-300,300)), p2 + (random.randint(1,300)), (random.randint(1,600)), color, random.randint(1,12))
 
-def create_points(count:int, lista):
-    point_list = arcade.SpriteList()
+def create_entities(count:int, lista, spriteName, scale, maxRandomAngle):
     for i in range(count):
-        point = arcade.Sprite("pixel-cig32x.png", 0.8, angle=random.randrange(0,360))
+        entity = arcade.Sprite(spriteName, scale, angle=random.randrange(0,maxRandomAngle))
 
         # Position the coin
-        point.center_x = random.randrange(SCREEN_WIDTH)
-        point.center_y = random.randrange(SCREEN_HEIGHT)
+        entity.center_x = random.randrange(SCREEN_WIDTH)
+        entity.center_y = random.randrange(SCREEN_HEIGHT)
 
         # Add the coin to the lists
-        lista.append(point)
+        lista.append(entity)
 class Jacket:
     def __init__(self, position_x, position_y, change_x, change_y):
 
@@ -35,7 +34,7 @@ class Jacket:
         self.position_y = position_y
         self.change_x = change_x
         self.change_y = change_y
-        self.sprite = arcade.Sprite("SprJacketHM1.webp", scale = 0.25, center_x=position_x, center_y=position_y)
+        self.sprite = arcade.Sprite("SprJacketHM1.webp", scale = 0.2, center_x=position_x, center_y=position_y)
     def on_update(self):
         self.sprite.position = [self.position_x, self.position_y]
         self.position_y += self.change_y
@@ -62,14 +61,17 @@ class MyGame(arcade.Window):
     def setup(self):
         self.jacket = Jacket(300, 300, 0, 0)
         self.points = arcade.SpriteList()
+        self.enemies = arcade.SpriteList()
         self.score = 0
-        create_points(50, self.points)
+        create_entities(50, self.points, "pixel-cig32x.png", 0.75, 360)
+        create_entities(25, self.enemies, "HM1sprRussianGangster.webp",0.3, 180)
 
     def on_draw(self):
         self.clear()
         arcade.set_background_color(arcade.color.ASH_GREY)
         draw_fire(0,800,arcade.color.BURNT_ORANGE)
         self.points.draw()
+        self.enemies.draw()
         self.jacket.sprite.draw()
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
@@ -82,6 +84,12 @@ class MyGame(arcade.Window):
         for point in points_hit_list:
             point.remove_from_sprite_lists()
             self.score += 1
+
+        self.enemies.update()
+        points_hit_list = arcade.check_for_collision_with_list(self.jacket.sprite, self.enemies)
+        for point in points_hit_list:
+            point.remove_from_sprite_lists()
+            self.score -= 1
 
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
